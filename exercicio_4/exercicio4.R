ac53c7ba4c3b161c688ef9256cbe4e605c2536b8
Victor Magalhães <victormmp@outlook.com>
2018-04-26 22:53:56 -0300

    update

--- exercicio_4/exercicio4.R
+++ exercicio_4/exercicio4.R
@@ -2,26 +2,37 @@ rm(list=ls())
 
 library("rgl")
 library("mlbench")
-library("simone")
+# library("simone")
 source("mds.R")
+aux <- gzfile("simone.rds")
+simone <- readRDS(aux)
 
 normalize <- function(array) {return((array-min(array))/(max(array)-min(array)))}
 
 data(BreastCancer)
-data(cancer)
+# data(cancer)
 usa <- (USArrests)
+bre <- (BreastCancer)
 
 usa$Murder <- normalize(usa$Murder)
 usa$Assault <- normalize(usa$Assault)
 usa$UrbanPop <- normalize(usa$UrbanPop)
 usa$Rape <- normalize(usa$Rape)
+# bre <- apply(data.matrix(BreastCancer),2,normalize)
+simone_aux <- simone$expr
+simone <- apply(data.matrix(simone_aux),2,normalize)
 
-bre <- apply(data.matrix(BreastCancer),2,normalize)
 
-yusa <- MDS(usa,0.001)
-ybreast <- MDS(usa,0.001)
+## Parte 1
 
-plot(yusa[,1],yusa[,2])
+yusa_mds2 <- MDS(usa,0.001)
+ycancer_mds2 <- MDS(simone,0.000001)
+
+yusa_mds3 <- MDS(usa,0.001,dimensions = 3)
+ycancer_mds3 <- MDS(simone,0.000001,dimensions = 3)
+
+plot(yusa_mds2[,1],yusa_mds2[,2])
+plot(ycancer_mds2[,1],ycancer_mds2[,2])
 
 
 
