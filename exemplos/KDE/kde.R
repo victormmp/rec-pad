15260bec7d234a74d58d1290fc312992aa5bb1ef
Victor Magalhães <victormmp@users.noreply.github.com>
2018-05-23 18:42:56 -0300

    Update kde.R

--- exemplos/KDE/kde.R
+++ exemplos/KDE/kde.R
@@ -1,6 +1,7 @@
 rm(list=ls())
 
 source("pxkde1var.R")
+library("ggplot2")
 
 x1 <- rnorm(100,mean=2,sd=0.2)
 x2 <- rnorm(100, mean=4, sd=0.2)
@@ -51,15 +52,17 @@ yzero <- rep(0,length(x1))
 px1 <- pxkde1var(xrange,x1)
 px2 <- pxkde1var(xrange,x2)
 
-pdfS <- px1 * t(px2)
+M <- matrix(nrow=length(xrange), ncol=length(xrange))
+
+for(i in 1:length(xrange)) {
+  for (j in 1:length(xrange)) {
+    M <- px1[i] * px1[j]
+  }
+}
 
 limiteY <- c(0,1)
 limiteX <- c(0,5)
 
 # pxCerto <- fnormal1var(xrange,mean(x),sd(x))
 
-plot(x,yzero,ylim=limiteY, xlim=limiteX)
-par(new=T)
-plot(xrange,px,type="l",ylim=limiteY,xlim=limiteX)
-par(new=T)
-plot(xrange,pxCerto,type="l",ylim=limiteY,xlim=limiteX, col="red")
+plot3d(xrange,xrage,M)
