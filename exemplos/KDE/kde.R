6bbe73edbb908aa238738051c602a5a7f8101a68
Victor Magalhães <victormmp@users.noreply.github.com>
2018-05-23 18:39:14 -0300

    Update kde.R

--- exemplos/KDE/kde.R
+++ exemplos/KDE/kde.R
@@ -1,1 +1,65 @@
-#
+rm(list=ls())
+
+source("pxkde1var.R")
+
+x1 <- rnorm(100,mean=2,sd=0.2)
+x2 <- rnorm(100, mean=4, sd=0.2)
+
+x <- c(x1,x2)
+
+# Para uma variavel
+
+xrange <- seq(0,6,0.001)
+
+yzero <- rep(0,length(x1))
+
+px <- pxkde1var(xrange,x1)
+
+limiteY <- c(0,2)
+limiteX <- c(0,5)
+
+pxCerto <- fnormal1var(xrange,2,0.2)
+par(mfrow=c(1,3))
+
+plot(x1,yzero,ylim=limiteY, xlim=limiteX)
+par(new=T)
+plot(xrange,px,type="l",ylim=limiteY,xlim=limiteX)
+par(new=T)
+plot(xrange,pxCerto,type="l",ylim=limiteY,xlim=limiteX, col="red")
+
+# Para duas variaveis
+
+yzero <- rep(0,length(x))
+
+px <- pxkde1var(xrange,x)
+
+limiteY <- c(0,1)
+limiteX <- c(0,5)
+
+pxCerto <- fnormal1var(xrange,mean(x),sd(x))
+
+plot(x,yzero,ylim=limiteY, xlim=limiteX)
+par(new=T)
+plot(xrange,px,type="l",ylim=limiteY,xlim=limiteX)
+par(new=T)
+plot(xrange,pxCerto,type="l",ylim=limiteY,xlim=limiteX, col="red")
+
+# Para uma variavel de dois parametros
+
+yzero <- rep(0,length(x1))
+
+px1 <- pxkde1var(xrange,x1)
+px2 <- pxkde1var(xrange,x2)
+
+pdfS <- px1 * t(px2)
+
+limiteY <- c(0,1)
+limiteX <- c(0,5)
+
+# pxCerto <- fnormal1var(xrange,mean(x),sd(x))
+
+plot(x,yzero,ylim=limiteY, xlim=limiteX)
+par(new=T)
+plot(xrange,px,type="l",ylim=limiteY,xlim=limiteX)
+par(new=T)
+plot(xrange,pxCerto,type="l",ylim=limiteY,xlim=limiteX, col="red")
